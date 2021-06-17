ca5a2bc7d9615579aac93136ca4971c00066e49d
ganeshkokkera <81211417+ganeshkokkera@users.noreply.github.com>
2021-06-17 15:02:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
+
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
-    
-      if (firstLetter === 'j') {
-        SpeakHello(names[i]);
-      }
-      else {
-        Speakbye(names[i]);
-      }
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
     }
-    
-
-})();
-    
+    else {
+      helloSpeaker(names[i]);
+    }
+  }
+  
+  })();
